c3a95090074fdca51b84669e6845092495f0a348
JYE <jo1814@naver.com>
2024-11-06 20:22:14 +0900

    [level 1] Title: 모의고사, Time: 3.18 ms, Memory: 36.7 MB -BaekjoonHub

---
+++ JavaScript/프로그래머스/1/42840. 모의고사/모의고사.js
@@ -0,0 +1,29 @@
+function solution(answers) {
+  class Dork {
+    constructor(id, answer, count) {
+      this.id = id;
+      this.answer = answer;
+      this.count = count;
+    }
+  }
+  let dork1 = new Dork(1, [1, 2, 3, 4, 5], 0);
+  let dork2 = new Dork(2, [2, 1, 2, 3, 2, 4, 2, 5], 0);
+  let dork3 = new Dork(3, [3, 3, 1, 1, 2, 2, 4, 4, 5, 5], 0);
+  let arr = [dork1, dork2, dork3];
+  let result = [];
+  let max;
+
+  arr.map((key) => {
+    for (let i = 0, j = 0; true; i++, j++) {
+      if (i >= key.answer.length) i = 0;
+      if (j >= answers.length) break;
+      if (key.answer[i] === answers[j]) key.count++;
+    }
+  });
+
+  max = Math.max(...arr.map((x) => x.count));
+  for (let key of arr) if (key.count === max) result.push(key.id);
+  return result;
+}
+
+solution([1, 2, 3, 4, 5]);
