a555fa64c3c317696ce181ba9988708354dbb3f6
조영은(Young-eun, Jo) <jo1814@naver.com>
2025-01-09 16:00:41 +0900

    [Bronze III] Title: 알고리즘 수업 - 알고리즘의 수행 시간 3, Time: 104 ms, Memory: 14220 KB -BaekjoonHub

---
+++ Java/백준/Bronze/24264. 알고리즘 수업 － 알고리즘의 수행 시간 3/알고리즘 수업 － 알고리즘의 수행 시간 3.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringBuilder sb = new StringBuilder();
+    long input = Integer.parseInt(br.readLine());
+
+    sb.append(input * input);
+
+    System.out.println(sb);
+    System.out.print(2);
+  }
+}
