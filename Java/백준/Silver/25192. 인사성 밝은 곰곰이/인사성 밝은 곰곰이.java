d4e1151111d5086ed163334f80ae383ff2bee6c9
조영은(Young-eun, Jo) <jo1814@naver.com>
2025-01-21 17:29:05 +0900

    [Silver IV] Title: 인사성 밝은 곰곰이, Time: 256 ms, Memory: 25696 KB -BaekjoonHub

---
+++ Java/백준/Silver/25192. 인사성 밝은 곰곰이/인사성 밝은 곰곰이.java
@@ -0,0 +1,43 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashSet;
+import java.util.Objects;
+
+public class Main {
+
+
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringBuilder sb = new StringBuilder();
+
+    int N = Integer.parseInt(br.readLine());
+
+    HashSet<String> set = new HashSet<>();
+
+    int answer = 0;
+
+    boolean start = false;
+
+    for (int i = 0; i < N; i++) {
+      String str = br.readLine();
+      if (Objects.equals(str, "ENTER")) {
+        set.clear();
+        start = true;
+        continue;
+      }
+
+      if (start) {
+        if (set.contains(str)) {
+          continue;
+        }
+        set.add(str);
+        answer++;
+      }
+    }
+
+    sb.append(answer);
+
+    System.out.print(sb);
+  }
+}
