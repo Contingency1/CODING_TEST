84ceccf57f3a5a8eab6b93c65539d76325c12b3c
조영은(Young-eun, Jo) <jo1814@naver.com>
2025-01-26 09:54:39 +0900

    [Silver III] Title: N과 M (2), Time: 108 ms, Memory: 14208 KB -BaekjoonHub

---
+++ Java/백준/Silver/15650. N과 M （2）/N과 M （2）.java
@@ -0,0 +1,53 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+public class Main {
+
+
+  private static void back(ArrayList<Integer> arr, int N, int M, StringBuilder sb) {
+
+    if (arr.size() == M) {
+      for (int i = 0; i < M; i++) {
+        sb.append(arr.get(i)).append(" ");
+      }
+      sb.append("\n");
+      return;
+    }
+
+    for (int i = 1; i <= N; i++) {
+
+      if (!arr.contains(i)) {
+        if (arr.isEmpty()) {
+          arr.add(i);
+          back(arr, N, M, sb);
+          arr.remove(arr.size() - 1);
+          continue;
+        }
+
+        if (arr.get(arr.size() - 1) < i) {
+          arr.add(i);
+          back(arr, N, M, sb);
+          arr.remove(arr.size() - 1);
+        }
+
+      }
+    }
+  }
+
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringBuilder sb = new StringBuilder();
+
+    String[] input = br.readLine().split(" ");
+    int N = Integer.parseInt(input[0]);
+    int M = Integer.parseInt(input[1]);
+
+    ArrayList<Integer> list = new ArrayList<>();
+    back(list, N, M, sb);
+
+    sb.deleteCharAt(sb.length() - 1);
+    System.out.print(sb);
+  }
+}
