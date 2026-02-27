6cb39ab3b84839cf9cdc92013729f2871eab44e5
조영은(Young-eun, Jo) <jo1814@naver.com>
2026-02-28 01:21:05 +0900

    [Bronze V] Title: Hook, Time: 100 ms, Memory: 13928 KB -BaekjoonHub

---
+++ Java/백준/Bronze/10189. Hook/Hook.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        // 문제에서 요구하는 4줄의 문자열을 그대로 출력합니다.
+        System.out.println("#  # #### #### #  #");
+        System.out.println("#### #  # #  # # #");
+        System.out.println("#### #  # #  # # #");
+        System.out.println("#  # #### #### #  #");
+    }
+}
